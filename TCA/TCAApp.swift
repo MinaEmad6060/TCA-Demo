5c00762dce301b58b649a292539c2a856320ac0f
Mina Emad <minaemad6060@gmail.com>
2025-12-08 13:15:49 +0200

    Initial Commit

---
+++ TCA/TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAApp.swift
+//  TCA
+//
+//  Created by Mina Emad on 08/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
